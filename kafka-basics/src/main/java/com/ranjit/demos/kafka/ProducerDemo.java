800488ae0f5553ae4de7082d282404eda60e29d0
ranjit <malickranjit72@gmail.com>
2025-08-02 17:58:59 +0530

    Add initial Kafka producer demo and project configuration files

---
+++ kafka-basics/src/main/java/com/ranjit/demos/kafka/ProducerDemo.java
@@ -0,0 +1,42 @@
+package com.ranjit.demos.kafka;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+
+public class ProducerDemo {
+
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        logger.info("Hello, Kafka Producer Demo!");
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // create the producer
+        try (Producer<String, String> producer = new KafkaProducer<>(properties)) {
+            //create a producer record
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("first_topic", "hello world");
+            //send data
+
+            producer.send(producerRecord);
+            producer.flush();
+        }
+        // flush and close the producer
+
+    }
+
+}
